492bb2fa80777eb7130c944c87382876f142504d
VarunAgarwal2004 <114917354+VarunAgarwal2004@users.noreply.github.com>
2024-08-07 20:34:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Blogs from "../src/sections/Blogs"
 function App() {
   return (
     <main className="min-h-screen h-fit w-full bg-dark flex flex-wrap justify-center gap-4 p-4">
-      <Blogs/>
     </main>
   );
 }
